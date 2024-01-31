aad6909094fcde9b8e11e0ea3e0211d1559dcd6d
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 01:35:10 +0600

    Change Dictionary Items and file

---
+++ Lab2/dictionaries/Change Dictionary Items.py
@@ -0,0 +1,15 @@
+#Example 1
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+#Example 2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
