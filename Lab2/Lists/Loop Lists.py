d6c3c4619a6a1807dd17766f66047b699999dc1a
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 00:08:39 +0600

    Loop Lists and file

---
+++ Lab2/Lists/Loop Lists.py
@@ -0,0 +1,17 @@
+#Example 1
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+#Example 2
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+#Example 3
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+#Example 4
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
