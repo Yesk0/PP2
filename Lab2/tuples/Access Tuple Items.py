640d827a13a47738a31a5c055fe0df5d36db83d3
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 00:46:30 +0600

    Access Tuple Items and file

---
+++ Lab2/tuples/Access Tuple Items.py
@@ -0,0 +1,22 @@
+#Example 1
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+#Example 2
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+#Example 3
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+#Example 4
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+#Example 5
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+#Example 6
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+#Example 7
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
