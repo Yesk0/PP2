2dd71260308fe956e568723172c5704f0c908e55
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 01:03:53 +0600

    Python Sets and file

---
+++ Lab2/sets/Python Sets.py
@@ -0,0 +1,32 @@
+myset = {"apple", "banana", "cherry"}
+#Example 1
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+#Example 2
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+#Example 3
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+#Example 4
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
+#Example 5
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+#Example 6
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+#Example 7
+set1 = {"abc", 34, True, 40, "male"}
+#Example 8
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+#Example 9
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
