eb5299b56930e61d8b387e959adceb70fe334f4a
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 01:43:19 +0600

    Loop Dictionaries and file

---
+++ Lab2/dictionaries/Loop Dictionaries.py
@@ -0,0 +1,15 @@
+#Example 1
+for x in thisdict:
+    print(x)
+#Example 2
+for x in thisdict:
+    print(thisdict[x])
+#Example 3
+for x in thisdict.values():
+    print(x)
+#Example 4
+for x in thisdict.keys():
+    print(x)
+#Example 5
+for x, y in thisdict.items():
+  print(x, y)
