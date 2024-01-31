e8e18eadea34ea60c61aade08dd991006002b6bf
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 01:36:47 +0600

    Add Dictionary Items and file

---
+++ Lab2/dictionaries/Add Dictionary Items.py
@@ -0,0 +1,15 @@
+#Example 1
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+#Example 2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
