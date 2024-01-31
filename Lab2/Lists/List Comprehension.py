5e46f194a41fdd14f89240a51de769dad32769da
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 00:17:35 +0600

    List Comprehension and file

---
+++ Lab2/Lists/List Comprehension.py
@@ -0,0 +1,33 @@
+#Example 1
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+#Example 2
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+#Example 3
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if x != "apple"]
+
+#Example 4
+newlist = [x for x in range(10)]
+
+#Example 5
+newlist = [x for x in range(10) if x < 5]
+
+#Example 6
+newlist = [x.upper() for x in fruits]
+
+#Example 7
+newlist = ['hello' for x in fruits]
+
+#Example 8
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
