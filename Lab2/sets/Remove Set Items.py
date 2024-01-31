0944ba32200e46c57a1dbcc786a63cb2adaf554a
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 01:11:39 +0600

    Remove Set Items and file

---
+++ Lab2/sets/Remove Set Items.py
@@ -0,0 +1,33 @@
+#Example 1
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+#Example 2
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+#Example 3
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+#Example 4
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+#Example 5
+thisset = {"apple", "banana", "cherry"}
+
+del thisset
+
+print(thisset)
+
