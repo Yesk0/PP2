23243381598f4495f0047467c5bf8093831a4794
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 00:26:37 +0600

    Copy Lists and file

---
+++ Lab2/Lists/Copy Lists.py
@@ -0,0 +1,8 @@
+#Example 1
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+#Example 2
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
