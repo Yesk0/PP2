18df48be45ef97d5c558cc365a2c4ac4f656ac96
Yesk0 <y_yerdaly@kbtu.kz>
2024-02-01 00:54:23 +0600

    Loop Tuples and file

---
+++ Lab2/tuples/Loop Tuples.py
@@ -0,0 +1,14 @@
+#Example 1
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+#Example 
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+#Example 3
+  thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
